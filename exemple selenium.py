69155a6d06f8d7402932c17966a82bf1ac9e5a3a
Ovni crea <messaline80@live.fr>
2020-06-10 22:41:21 +0200

    Add files via upload

---
+++ exemple selenium.py
@@ -0,0 +1,13 @@
+def bypass_cloudflare(url):
+    driverPath = get_driver_path('chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=Choisir le dossier pour suvegarder")
+    driver = webdriver.Chrome(driverPath, chrome_options=options)
+    options.headless = False
+    driver.get(url)
+    time.sleep(10)
+    page_source = driver.page_source
+    driver.close()
+    print(page_source)
+    sHtmlContent = page_source
+    return sHtmlContent
