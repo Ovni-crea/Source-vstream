ebf1d02b89a598d2917e14ffce00ac664e87ab9f
Ovni crea <messaline80@live.fr>
2020-06-13 16:35:25 +0200

    Update exemple selenium linux.py

--- exemple selenium linux.py
+++ exemple selenium linux.py
@@ -6,7 +6,7 @@ def bypass_cloudflare_linux(url):
     options.headless = False
     driver.get(url)
     time.sleep(10)
-    page_source = (driver.page_source).encode('utf-8')
+    page_source = (driver.page_source).encode('utf-8', errors='replace')
     driver.close()
     print(page_source)
     sHtmlContent = page_source
