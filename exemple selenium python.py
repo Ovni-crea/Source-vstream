4354572ec5d40f1e1930d605b78c99ccf9c81a90
Ovni crea <messaline80@live.fr>
2020-06-13 13:52:29 +0200

    Create exemple selenium python.py

---
+++ exemple selenium python.py
@@ -0,0 +1,13 @@
+def bypass_cloudflare_linux(url):
+    driverPath = get_driver_path('chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_argument("user-data-dir=/home/USERNAME/chrome")
+    driver = webdriver.Chrome(driverPath, chrome_options=options)
+    options.headless = False
+    driver.get(url)
+    time.sleep(10)
+    page_source = (driver.page_source).encode('utf-8')
+    driver.close()
+    print(page_source)
+    sHtmlContent = page_source
+    return sHtmlContent
