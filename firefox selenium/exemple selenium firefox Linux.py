7aa0174713a1d1c9f7aaec2484e430bc1d7a3d47
Ovni crea <messaline80@live.fr>
2020-06-22 23:02:28 +0200

    Update exemple selenium firefox Linux.py

--- firefox selenium/exemple selenium firefox Linux.py
+++ firefox selenium/exemple selenium firefox Linux.py
@@ -5,14 +5,14 @@ import os
 
 
 def bypass_cloudflare(url):
-	os.chmod('/home/USERNAME/.kodi/addons/script.module.selenium/bin/geckodriver/linux64/geckodriver/geckodriver', 0755)
-    	path = "/home/USERNAME/.kodi/addons/script.module.selenium/bin/geckodriver/linux64/geckodriver/geckodriver"
-    	options.headless = False
-    	browser = webdriver.Firefox(executable_path=path, options=options, log_path="/home/USERNAME/geckodriver.log")
-    	browser.get(url)
-    	time.sleep(10)
-    	page_source = (driver.page_source).encode('utf-8', errors='replace')
-    	driver.close()
-    	print(page_source)
-    	sHtmlContent = page_source
-    	return sHtmlContent
+    os.chmod('/home/USERNAME/.kodi/addons/script.module.selenium/bin/geckodriver/linux64/geckodriver/geckodriver', 0755)
+    path = "/home/USERNAME/.kodi/addons/script.module.selenium/bin/geckodriver/linux64/geckodriver/geckodriver"
+    options.headless = False
+    browser = webdriver.Firefox(executable_path=path, options=options, log_path="/home/USERNAME/geckodriver.log")
+    browser.get(url)
+    time.sleep(10)
+    page_source = (driver.page_source).encode('utf-8', errors='replace')
+    driver.close()
+    print(page_source)
+    sHtmlContent = page_source
+    return sHtmlContent
